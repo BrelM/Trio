9f8413b7e358672a1eb19878de7738ab320c0d79
Muzafari Sayed Basharat Reza <150054270+SayedBM@users.noreply.github.com>
2025-12-08 16:45:20 +0100

    Update Competence.java
    Numbre to UE replacement

--- Competence.java
+++ Competence.java
@@ -2,32 +2,32 @@ package Projet;
 
 public class Competence {
     private String type;
-    private int linkedNumber1;
-    private int linkedNumber2;
+    private int linkedUE1;
+    private int linkedUE2;
 
     public Competence() {}
 
-    public Competence(String type, int linkedNumber1, int linkedNumber2) {
+    public Competence(String type, int linkedUE, int linkedUE2) {
         this.type = type;
-        this.linkedNumber1 = linkedNumber1;
-        this.linkedNumber2 = linkedNumber2;
+        this.linkedUE1 = linkedUE1;
+        this.linkedUE2 = linkedUE2;
     }
 
     public String getType() { return type; }
     public void setType(String type) { this.type = type; }
 
-    public int getLinkedNumber1() { return linkedNumber1; }
-    public void setLinkedNumber1(int linkedNumber1) { this.linkedNumber1 = linkedNumber1; }
+    public int getLinkedUE1() { return linkedUE1; }
+    public void setLinkedUE1(int linkedUE1) { this.linkedUE1 = linkedUE1; }
 
-    public int getLinkedNumber2() { return linkedNumber2; }
-    public void setLinkedNumber2(int linkedNumber2) { this.linkedNumber2 = linkedNumber2; }
+    public int getLinkedUE2() { return linkedUE2; }
+    public void setLinkedUE2(int linkedUE2) { this.linkedUE2 = linkedUE2; }
 
     @Override
     public String toString() {
         return "Competence{" +
                "type='" + type + '\'' +
-               ", linkedNumber1=" + linkedNumber1 +
-               ", linkedNumber2=" + linkedNumber2 +
+               ", linkedUE1=" + linkedUE1 +
+               ", linkedUE2=" + linkedUE2 +
                '}';
     }
 }
